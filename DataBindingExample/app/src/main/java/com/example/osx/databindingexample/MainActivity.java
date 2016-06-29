9994b12a950eb000ddbf56184e24ea94346f67bd
Hoa Vo Quang <hvquang@openwt.com>
2016-06-29 11:55:26 +0700

    Add Data binding

---
+++ DataBindingExample/app/src/main/java/com/example/osx/databindingexample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.osx.databindingexample;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.osx.databindingexample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "Hoa");
+        binding.setUser(user);
+    }
+}
